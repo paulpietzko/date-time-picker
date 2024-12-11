b209ce9a86410e518b5c3597b8b7e27023c14c72
paulpietzko <paul.pietzko@icloud.com>
2024-12-11 11:23:17 +0100

    add date-time-picker component with basic template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { DateTimePickerComponent } from './date-time-picker/date-time-picker.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [DateTimePickerComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/date-time-picker/date-time-picker.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-date-time-picker',
+  imports: [],
+  templateUrl: './date-time-picker.component.html',
+  styleUrl: './date-time-picker.component.scss'
+})
+export class DateTimePickerComponent {
+
+}
