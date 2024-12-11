b690390be87827174adf4721f2403b3e7d9a4564
paulpietzko <paul.pietzko@icloud.com>
2024-12-11 13:18:39 +0100

    add dayjs library

--- src/app/components/time-picker/time-picker.component.ts
+++ src/app/components/time-picker/time-picker.component.ts
@@ -2,13 +2,16 @@ import { Component } from '@angular/core';
 import {MatTimepickerModule} from '@angular/material/timepicker';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
+import dayjs, { Dayjs } from 'dayjs';
+import { MatDatepickerModule } from '@angular/material/datepicker';
 
 @Component({
   selector: 'app-time-picker',
-  imports: [MatTimepickerModule, MatFormFieldModule, MatInputModule],
+  imports: [MatTimepickerModule, MatFormFieldModule, MatInputModule,
+    MatDatepickerModule],
   templateUrl: './time-picker.component.html',
   styleUrl: './time-picker.component.scss'
 })
 export class TimePickerComponent {
-
+  
 }
