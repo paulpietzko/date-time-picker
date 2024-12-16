572103bea8ac25991866f364d74953a0c2dab38d
paulpietzko <paul.pietzko@icloud.com>
2024-12-16 10:27:56 +0100

    refactor date-time-picker component for improved structure and change detection; update styles for better layout

--- src/app/components/date-time-picker/date-time-picker.component.ts
+++ src/app/components/date-time-picker/date-time-picker.component.ts
@@ -1,8 +1,8 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, effect, OnInit, viewChild } from '@angular/core';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatDatepickerModule } from '@angular/material/datepicker';
-import { MatTimepickerModule } from '@angular/material/timepicker';
+import { MatTimepicker, MatTimepickerModule } from '@angular/material/timepicker';
 import { FormsModule } from '@angular/forms';
 import { MAT_DAYJS_DATE_ADAPTER_OPTIONS, MAT_DAYJS_DATE_FORMATS, MatDayjsDateAdapter, MatDayjsModule } from '../../shared/date-adapters/date-adapter-api';
 import { DateAdapter, MAT_DATE_LOCALE, MAT_DATE_FORMATS, provideNativeDateAdapter } from '@angular/material/core';
@@ -11,16 +11,16 @@ import { DateAdapter, MAT_DATE_LOCALE, MAT_DATE_FORMATS, provideNativeDateAdapte
   selector: 'app-date-time-picker',
   templateUrl: './date-time-picker.component.html',
   styleUrls: ['./date-time-picker.component.scss'],
-  // providers: [
-  //   {
-  //     provide: DateAdapter,
-  //     useClass: MatDayjsDateAdapter,
-  //     deps: [MAT_DATE_LOCALE, MAT_DAYJS_DATE_ADAPTER_OPTIONS],
-  //   },
-  //   { provide: MAT_DATE_FORMATS, useValue: MAT_DAYJS_DATE_FORMATS },
-  //   { provide: MAT_DAYJS_DATE_ADAPTER_OPTIONS, useValue: { useUtc: true } },
-  // ],
-  providers: [provideNativeDateAdapter()],
+  providers: [
+    {
+      provide: DateAdapter,
+      useClass: MatDayjsDateAdapter,
+      deps: [MAT_DATE_LOCALE, MAT_DAYJS_DATE_ADAPTER_OPTIONS],
+    },
+    { provide: MAT_DATE_FORMATS, useValue: MAT_DAYJS_DATE_FORMATS },
+    { provide: MAT_DAYJS_DATE_ADAPTER_OPTIONS, useValue: { useUtc: true } },
+  ],
+  // providers: [provideNativeDateAdapter()],
   imports: [
     MatFormFieldModule,
     MatInputModule,
@@ -31,18 +31,22 @@ import { DateAdapter, MAT_DATE_LOCALE, MAT_DATE_FORMATS, provideNativeDateAdapte
   ],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class DateTimePickerComponent {
+export class DateTimePickerComponent implements OnInit {
   timeChanged: boolean = false;
   dateChanged: boolean = false;
   value: Date = new Date();
 
-  setTimeChanged() {
-    console.log('here');
-    this.timeChanged = true;
+  myComponentRef = viewChild.required(MatTimepicker);
+
+  constructor() { }
+
+  ngOnInit(): void {
+    const x = this.myComponentRef().selected.subscribe(value => {
+      this.timeChanged = true;
+    });
   }
 
   setDateChanged() {
     this.dateChanged = true;
   }
-
 }
